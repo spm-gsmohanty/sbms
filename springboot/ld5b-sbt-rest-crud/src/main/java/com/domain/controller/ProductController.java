329b7247361529c97dfba0e83753c843e7794abc
livegit2 <livegit2@gmail.com>
2021-04-03 10:33:36 +0530

    .

--- springboot/ld5b-sbt-rest-crud/src/main/java/com/domain/controller/ProductController.java
+++ springboot/ld5b-sbt-rest-crud/src/main/java/com/domain/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.domain.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -57,6 +58,13 @@ public class ProductController {
 		Product product2 = productRepository.save(product1);
 		System.out.println("Updated : " + product);
 		return product2;
+	}
+	
+	@DeleteMapping("/products/{id}")
+	public boolean deleteProduct(@PathVariable(value = "id") Long productId) {
+		productRepository.deleteById(productId);
+		System.out.println("Deleted : " + productId);
+		return true;
 	}
 	
 }
